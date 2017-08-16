e43bd25d904e4896b3cb9053b3e94260b17a92f4
郭佳 <guo_jia_jia@hotmail.com>
2017-08-16 09:05:58 +0800

    添加信息

---
+++ WebSmall/News/utils/api.js
@@ -0,0 +1,25 @@
+/**
+ * 所有的API接口地址都在这里定义
+ */
+/**
+ * 百度翻译API接口地址
+ * @type {String}
+ */
+const BAIDU_FANYI_URL = 'https://fanyi-api.baidu.com/api/trans/vip/translate';
+/**
+ * 百度翻译APPId
+ * @type {String}
+ */
+const BAIDU_FANYI_APPID = '20160314000015486'
+/**
+ * 百度翻译APP密钥
+ * @type {String}
+ */
+const BAIDU_FANYI_APPSCREET = '21ynQvdwDW0TmxpQVlxg'
+
+
+module.exports = {
+  BAIDU_FANYI_URL: BAIDU_FANYI_URL,
+  BAIDU_FANYI_APPID:BAIDU_FANYI_APPID,
+  BAIDU_FANYI_APPSCREET:BAIDU_FANYI_APPSCREET
+}
---
+++ WebSmall/News/views/lishi/lishi.js
@@ -0,0 +1,48 @@
+Page({
+    onShareAppMessage: function (res) {
+        if (res.from === 'button') {
+            // 来自页面内转发按钮
+            console.log(res.target)
+        }
+        return {
+            title: '中英互译',
+            path: 'views/index/index',
+            success: function (res) {
+                // 转发成功
+            },
+            fail: function (res) {
+                // 转发失败
+            }
+        }
+    },
+  data:{
+    text:"历史页面",
+    fanyi_lishi:[]
+  },
+  onLoad (options){
+  },
+  onReady (){
+    // 页面渲染完成
+  },
+  onShow (){
+    // 页面显示
+    var that = this
+    wx.getStorage({
+        key: "lishiAll",
+  	success (res){
+  	  that.setData({
+  	  	fanyi_lishi:res.data
+  	  })
+  	},
+  	fail (){
+  	  	console.log('读取数据失败!')
+  	}
+  	})
+  },
+  onHide (){
+    // 页面隐藏
+  },
+  onUnload (){
+    // 页面关闭
+  }
+})
