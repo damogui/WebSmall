2e77e92a9196166ee0316da26982bbceaca45f20
郭佳 <guo_jia_jia@hotmail.com>
2017-08-16 11:52:52 +0800

    seo

--- WebSmall/News/app.js
+++ WebSmall/News/app.js
@@ -1,15 +1,31 @@
-//App() 函数用来注册一个小程序。接受一个 object 参数，其指定小程序的生命周期函数等
+//app.js
 App({
-  //生命周期函数--监听小程序初始化
-  onLaunch () {
-
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
   },
-  //生命周期函数--监听小程序显示
-  onShow () {
-    
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
   },
-  //生命周期函数--监听小程序隐藏
-  onHide () {
-    
+  globalData:{
+    // 登录
+    hasLogin: false,
   }
 })
