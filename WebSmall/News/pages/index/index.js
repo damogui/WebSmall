221d426bf59887ff1be44ad35d4110adc7b021d8
郭佳 <guo_jia_jia@hotmail.com>
2017-08-18 16:04:48 +0800

    处理弹窗

--- WebSmall/News/pages/index/index.js
+++ WebSmall/News/pages/index/index.js
@@ -65,7 +65,7 @@ function GetData() {
         success: function (res) {
             //console.log(res.data);
             var xyd = res.data.split('=')[1];
-            item2 = "信:" + xyd.split(',')[3];
+            item2 = "xin:" + xyd.split(',')[3];
             itemall.push(item2);
             wx.setStorage({
                 key: "gdata",
@@ -90,7 +90,7 @@ function GetData() {
 Page(
     {
     onLoad: function () {
-        GetData();
+       // GetData();
     },data: {
         img_urls: [
             "https://www.our666.com//GoodUI/img/yg.jpg",
@@ -103,28 +103,90 @@ Page(
         duration: 2000,
 
         contents: [0, 1], //2, 3, 4, 5
-        // items: GetDataShow(),
+         items:[0,1],// GetDataShow(),
         new_pic: [
             "https://www.our666.com//GoodUI/img/yg.jpg",
             "https://www.our666.com//GoodUI/img/cn.png"
         ]
-    },
+    },item_click(e){
+        
+             // console.log("修改");
+                    //请求服务器
+            wx.request({
+                url: API_URL1,
+                data: {
+        
+                },
+                method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+                header: {
+                    'content-type': 'application/json'
+                }, // 设置请求的 header
+                success: function (res) {
+        
+                    //console.log("data-id:"+e.target.dataset.id);
+        
+                   // console.log(res.data);
+                    var hq_str_sz300059 = res.data.split('=')[1];
+                    item1 = "dong:" + hq_str_sz300059.split(',')[3];
+                    //console.log(item1);
+
+                    alert(item1)
+                    
+            //  wx.showtoast({
+            //    title: item1,
+            //    icon: 'loading',
+            //    duration: 10000
+            // });
+        
+                },
+                fail: function () {
+                    console.log("请求失败");
+                    
+                    wx.showToast({
+               title: '请求失败',
+               icon: 'loading',
+               duration: 10000
+            });
+                    // fail
+                    // wx.hideToast();
+                },
+                complete: function () {
+                    // complete
+                }
+            });
+           
+            },
     onReady() {
 
-        // 页面渲染完成
-        var that = this;
-        wx.getStorage({
-            key: "gdata",
-            success(res) {
-                if (res.data != null) {
-                that.setData({
-                    items: res.data//res.data
-                   
-                });
-            }
-                //itemall=res.data;
 
-            }
-        })
+        console.log("渲染完成");
+        // 页面渲染完成
+        // var that = this;
+        // wx.getStorage({
+        //     key: "gdata",
+        //     success(res) {
+        //         if (res.data != null) {
+        //         that.setData({
+        //             items: res.data//res.data
+                   
+        //         });
+        //     }
+        //         //itemall=res.data;
+
+        //     }
+        // })
     }
-})
+        }
+        
+        )
+
+     //自己的处理消息
+     
+     function  alert(obj){
+
+        wx.showToast({
+            title: obj,
+            icon: 'loading',
+            duration: 1000
+     })
+    }
