a0a4e7572fbb0462bf33c82623b0febcb6e64d64
郭佳 <guo_jia_jia@hotmail.com>
2017-10-25 14:59:57 +0800

    seo

---
+++ WebSmall/Test/test.js
@@ -0,0 +1,48 @@
+﻿
+var module = {
+    init: function () {
+        //todo 逻辑函数
+        this.render();
+        this.initBtns();
+    },
+
+    render: function () {
+   
+     
+
+    },
+    initBtns: function () {
+        //todo 绑定事件
+      
+        //确定重置
+        $("body").delegate("#reSetOk", "click", function () {
+
+            //提交表单
+            $.ajax({
+                type: "post",
+                url: "",
+                dataType: "json",
+                data: {
+
+                    stuId: stuId
+                },
+                success: function (data) {
+                 
+
+                }
+            });
+
+
+        });
+
+    }
+
+
+};
+
+
+//绑定数据
+$(function () {
+    module.init();
+
+});
