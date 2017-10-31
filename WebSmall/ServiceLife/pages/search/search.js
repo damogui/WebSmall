d821b483d0657144ae8f1b665e27386ca42b6de2
郭佳 <guo_jia_jia@hotmail.com>
2017-10-31 16:22:42 +0800

    seoseo

--- WebSmall/ServiceLife/pages/search/search.js
+++ WebSmall/ServiceLife/pages/search/search.js
@@ -1,11 +1,21 @@
 var bmap = require('../../libs/bmap-wx.min.js');
 var wxMarkerData = [];
+var queryStr = "公园";
 Page({
     data: {
         markers: [],
         latitude: '',
         longitude: '',
-        placeData: {}
+        placeData: {},
+
+        textarea_placeholder: "请输入要搜索的内容",
+        btn_text: "搜索",
+
+        textarea_text: "",
+        result_text: "nothing...",
+        result: "结果",
+        fanyi_lishi: [],
+        fanyi_src: "../../img/fanyi.png"
     },
     makertap: function(e) {
         var that = this;
@@ -14,6 +24,10 @@ Page({
         that.changeMarkerColor(wxMarkerData, id);
     },
     onLoad: function() {
+
+        // if (txt.toString.length == 0 || txt == null) {
+        //     txt = "公园";
+        // }
         var that = this;
         var BMap = new bmap.BMapWX({
             ak: 'SUEqSOtxTbhlIOXFktXEvWEoIuElDE4Y'
@@ -22,11 +36,13 @@ Page({
             console.log(data)
         };
         var success = function(data) {
+
             wxMarkerData = data.wxMarkerData;
             that.setData({
                 markers: wxMarkerData
             });
             that.setData({
+
                 latitude: wxMarkerData[0].latitude
             });
             that.setData({
@@ -34,7 +50,7 @@ Page({
             });
         }
         BMap.search({
-            "query": '美食',
+            "query": queryStr,
             fail: fail,
             success: success,
             iconPath: '../../img/marker_red.png',
@@ -65,5 +81,30 @@ Page({
         that.setData({
             markers: markersTemp
         });
+    },
+    bindTextAreaBlur(res) {
+        this.setData({
+            textarea_text: res.detail.value,
+            fanyi_src: '../../img/fanyi.png'
+        })
+    },
+    btn_click() {
+        let text = this.data.textarea_text;
+        if (text == '' || text == null) {
+            wx.showToast({
+                title: '请输入要搜索的内容', //提示的内容
+                icon: 'success', //图标，只支持"success"、"loading"
+                duration: 2000, //提示的延迟时间，单位毫秒，默认：1500, 最大为10000
+            })
+            return;
+        }
+        // const that = this;
+        // var salt = (new Date).getTime();
+        // var appid = api.BAIDU_FANYI_APPID
+        // var str = appid + text + salt + api.BAIDU_FANYI_APPSCREET;
+        // var sign = md5.MD5(str);
+
+        queryStr = text;
+        this.onLoad();
     }
 })
