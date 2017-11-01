69ec4433050dd6d9825eec8b11a41d4d95c5f511
郭佳 <guo_jia_jia@hotmail.com>
2017-11-01 11:00:58 +0800

    玩笑小程序

--- WebSmall/MapNav/pages/index/index.js
+++ WebSmall/MapNav/pages/index/index.js
@@ -2,33 +2,42 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    motto: '微信小程序尝鲜，地图定位',
-    userInfo: {},
-    appInfo:{
-      logoUrl:'../../image/logo.png',
-      title:'使用微信内置地图查看API定位'
+    data: {
+        motto: '微信小程序尝鲜，地图定位',
+        userInfo: {},
+        appInfo: {
+            logoUrl: '../../image/logo.png',
+            title: '使用微信内置地图查看API定位'
+        }
+    },
+    //事件处理函数
+    bindViewTap: function() {
+        debugger;
+        wx.navigateTo({
+            // url: "../location/location"
+            url: "../location/location", //url跳转地址
+            success: function(res) {
+                console.log(res)
+            },
+            fail: function(res) {
+                console.log(res)
+            }
+
+        })
+    },
+    onLoad: function() {
+        console.log('onLoad')
+        var that = this
+        that.setData({
+                appInfo: this.data.appInfo
+            })
+            //调用应用实例的方法获取全局数据
+            // app.getUserInfo(function(userInfo){
+            //   //更新数据
+            //   that.setData({
+            //     userInfo:userInfo
+            //   })
+            //   that.update()
+            // })
     }
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../location/location'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    that.setData({
-        appInfo:this.data.appInfo
-    })
-  	//调用应用实例的方法获取全局数据
-    // app.getUserInfo(function(userInfo){
-    //   //更新数据
-    //   that.setData({
-    //     userInfo:userInfo
-    //   })
-    //   that.update()
-    // })
-  }
 })
