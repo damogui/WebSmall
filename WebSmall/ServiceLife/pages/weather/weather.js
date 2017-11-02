099efea9d6d7518e2aa805330fcd07a2a5eb317a
郭佳 <guo_jia_jia@hotmail.com>
2017-11-02 10:44:20 +0800

    seo

--- WebSmall/ServiceLife/pages/weather/weather.js
+++ WebSmall/ServiceLife/pages/weather/weather.js
@@ -29,6 +29,7 @@ Page({
             console.log('fail!!!!')
         };
         var success = function(data) {
+
             console.log('success!!!');
             var weatherData = data.currentWeather[0];
             weatherData = '城市：' + weatherData.currentCity + '\n' + 'PM2.5：' + weatherData.pm25 + '\n' + '日期：' + weatherData.date + '\n' + '温度：' + weatherData.temperature + '\n' + '天气：' + weatherData.weatherDesc + '\n' + '风力：' + weatherData.wind + '\n';
