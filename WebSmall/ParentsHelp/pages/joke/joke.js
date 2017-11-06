34a961055e9c8596954ca0e7f70efea238a144d2
郭佳 <guo_jia_jia@hotmail.com>
2017-11-06 19:28:10 +0800

    seo

--- WebSmall/ParentsHelp/pages/joke/joke.js
+++ WebSmall/ParentsHelp/pages/joke/joke.js
@@ -1,6 +1,6 @@
 var http = require('../../utils/util')
 var app = getApp()
-var url = 'https://japi.juhe.cn/joke/content/text.from'
+var url = 'https://www.our666.com//Data/GetOrgsList';
 
 Page({
     onShareAppMessage: function(res) {
@@ -9,7 +9,7 @@ Page({
             console.log(res.target)
         }
         return {
-            title: '笑话',
+            title: '搜索',
             path: "pages/joke/joke",
             success: function(res) {
                 // 转发成功
@@ -24,21 +24,47 @@ Page({
         loadingHide: false,
         hideFooter: true,
         jokeList: [],
+        TotalPage: 1 //总页数
     },
     onLoad: function(options) {
+
         // 页面初始化 options为页面跳转所带来的参数
         var that = this
-            //请求笑话列表
-        http.request(url, this.data.page, function(dataJson) {
-            that.setData({
-                jokeList: that.data.jokeList.concat(dataJson.result.data),
-                loadingHide: true
-            })
-        }, function(reason) {
-            console.log(reason)
-            that.setData({
-                loadingHide: true
-            })
+            //请求列表
+        wx.request({
+            url: url,
+            data: {
+                currentPage: this.data.page,
+                pageSize: 10,
+                type: 3
+            },
+            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+            header: {
+                'content-type': 'application/json'
+            }, // 设置请求的 header
+            success: function(res) {
+
+                that.setData({
+                    jokeList: that.data.jokeList.concat(res.data.Data),
+                    loadingHide: true,
+                    TotalPage: parseInt((res.data.TotalCount / 10)) + 1
+
+
+                })
+
+
+                wx.hideToast();
+
+
+            },
+            fail: function() {
+
+                console.log("失败");
+
+            },
+            complete: function() {
+
+            }
         })
 
     },
@@ -47,20 +73,7 @@ Page({
      * 下拉刷新
      */
     onPullDownRefresh() {
-        // var that = this
-        // this.setData( {
-        //   page: 1,
-        //   jokeList:[]
-        // })
-        // http.request( url, this.data.page, function( dataJson ) {
-        //   that.setData( {
-        //     jokeList: that.data.jokeList.concat( dataJson.result.data )
-        //   })
-        //   wx.stopPullDownRefresh()
-        // }, function( reason ) {
-        //   console.log( reason )
-        //   wx.stopPullDownRefresh()
-        // })
+
     },
 
     /**
@@ -69,23 +82,61 @@ Page({
     loadMore() {
         //请求笑话列表
         var that = this
+
+
+
+        if (this.data.page >= this.data.TotalPage) {
+
+            that.setData({
+                    page: this.data.TotalPage
+                })
+                // hideFooter: true;
+            return; //大于当前分页，返回
+        } else {
             //显示footer
-        this.setData({
+            this.setData({
                 hideFooter: !this.data.hideFooter
             })
-            //请求笑话列表
-        http.request(url, ++this.data.page, function(dataJson) {
-            that.setData({
-                jokeList: that.data.jokeList.concat(dataJson.result.data),
-                hideFooter: !that.data.hideFooter
+            wx.request({
+                url: url,
+                data: {
+                    currentPage: ++this.data.page,
+                    pageSize: 10,
+                    type: 3
+                },
+                method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+                header: {
+                    'content-type': 'application/json'
+                }, // 设置请求的 header
+                success: function(res) {
+
+                    that.setData({
+                        jokeList: that.data.jokeList.concat(res.data.Data),
+                        hideFooter: !that.data.hideFooter
+                    })
+
+                    //console.log("成功" + res+res.data);
+                    // success
+                    wx.hideToast();
+                    // console.log('服务器返回' + res.data);
+
+                },
+                fail: function() {
+
+                    console.log("失败");
+
+                },
+                complete: function() {
+
+                }
             })
 
-        }, function(reason) {
-            console.log(reason)
-            that.setData({
-                hideFooter: !that.data.hideFooter
-            })
-        })
+
+        }
+
+
+
+
 
 
     },
